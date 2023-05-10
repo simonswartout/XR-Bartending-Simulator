067c57ffe9d3e8ce596e04824b802301ca0054c1
simonswartout <simon.swartout@gmail.com>
2023-05-09 23:13:25 -0400

    Attempt at Spatial Anchors and Imported TMP

---
+++ Assets/DungeonCrawler/Scripts/LoadSpatialAnchors.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class LoadSpatialAnchors : MonoBehaviour
+{
+    public SpatialAnchorLoader spatialAnchorLoader;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
